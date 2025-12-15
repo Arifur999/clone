1d11bbf1f479ad62c4549e6a257291d2e0fbd64d
Arifur999 <arr96777@gmail.com>
2025-12-15 23:56:20 +0600

    added notification

---
+++ src/Notifications.jsx
@@ -0,0 +1,31 @@
+function Notifications() {
+  const notifications = [
+    {
+      id: 1,
+      title: "New Login Detected",
+      message: "A user just logged in from a new device.",
+      time: "2 minutes ago",
+      type: "info",
+    },
+    {
+      id: 2,
+      title: "New Product Added",
+      message: "A new product has been added to the database.",
+      time: "10 minutes ago",
+      type: "success",
+    },
+    {
+      id: 3,
+      title: "High Traffic Alert",
+      message: "Your site is receiving unusually high traffic.",
+      time: "1 hour ago",
+      type: "warning",
+    },
+    {
+      id: 4,
+      title: "System Update",
+      message: "Backend server was restarted successfully.",
+      time: "Yesterday",
+      type: "info",
+    },
+  ];}
